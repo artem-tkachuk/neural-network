d3acaf1e44ba88629371829f34a22662dbf8d73d
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-09-07 00:13:22 -0700

    Might be the right results actually. It just perfectly learns to identify one class and does has no idea about the second???

--- training/train.py
+++ training/train.py
@@ -16,22 +16,31 @@ from graph.graph import init, replot
 def train(fn, nTimes, rate, mh):
 
     fileName = f'data/train/{fn}-train.txt'
-    data, n, mh = readFile(fileName)
+    data, n, mx = readFile(fileName)     # of training examples and neurons in the hidden layer
 
     features = data[:, :-1]              # matrix of training examples
-    labels = data[:, -1]  # vector of corresponding labels
-    y_hats = np.empty(labels.shape)
+    labels = data[:, -1]                 #vector of corresponding labels
+    y_hats = np.empty(labels.shape)      #vector of predictions
 
-    thetas_y_hat = np.zeros((mh))         #parameters for input layer
+    thetas_h = np.zeros((mx, mh))         #parameters for hidden layer
+    thetas_y_hat = np.zeros((mh))         #parameters for hidden layer
+
 
     fig, ax, xdata, ydata, line = init(fn)     #plotting the log likelihood while training
 
     for k in range(nTimes):
+        gradient_h = np.zeros((mx, mh))
         gradient_y_hat = np.zeros((mh))
 
         for example in range(n):
             # Forward Pass, computing hidden layer
-            h, y = features[example], labels[example]
+            x, y, h = features[example], labels[example], np.empty((mh))
+
+            for j in range(mh):
+                sum = 0.0
+                for i in range(mx):
+                    sum += x[i] * thetas_h[i][j]
+                h[j] = sigmoid(sum)
 
             #computing prediction
             sum = 0
@@ -45,9 +54,17 @@ def train(fn, nTimes, rate, mh):
             for j in range(mh):
                 gradient_y_hat[j] += delta * h[j]
 
+            for i in range(mx):
+                for j in range(mh):
+                    gradient_h[i][j] += delta * h[j] * (1 - h[j]) * thetas_y_hat[j] * x[i]
+
         # updating parameters
         for j in range(mh):
             thetas_y_hat[j] += rate * gradient_y_hat[j]
+
+        for i in range(mx):
+            for j in range(mh):
+                thetas_h[i][j] += rate * gradient_h[i][j]
 
 
         LL = logLikelihood(labels, y_hats)
@@ -57,7 +74,7 @@ def train(fn, nTimes, rate, mh):
 
     plt.savefig(f'graph/pics/{fn}.png')
 
-    return thetas_y_hat
+    return (thetas_h, thetas_y_hat, mh)
 
 # TODO loop for many h layers here? Or a matrix thing is possible with multiple layers too? Yes! 3d theta where i-th grid is current h
 # TODO make it a vector of size n, if there are n hidden layers in the network
